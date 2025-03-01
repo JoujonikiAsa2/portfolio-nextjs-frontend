36d6dab3b7c0c764b914da2b7380b6e4cc1a2356
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-03-02 01:32:37 +0600

    Project Initiated

---
+++ src/app/font.ts
@@ -0,0 +1,12 @@
+import { Roboto_Mono, Rajdhani } from 'next/font/google'
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const rajdhani = Rajdhani({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
