0d1a441797ae9cf1db5578e7504a38052d37d22b
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-03-03 19:02:58 +0600

    added custom loader

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../app/globals.css'
+const Loader = () => {
+    return (
+        <div className="flex items-center justify-center h-screen bg-white">
+      <div className="loader"></div>
+    </div>
+    );
+};
+
+export default Loader;
